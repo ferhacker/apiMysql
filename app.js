efda150de378100cba3c7c957cc759c10097e47c
Fernando <111725336+ferhacker@users.noreply.github.com>
2024-12-05 13:25:07 -0300

    porta3306.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3306;
 const cors = require('cors');
 const mysql = require('mysql');
 
