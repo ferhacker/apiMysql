371dd5bfb1d19e823f0a4370d8a0f561e2a7af42
Fernando <111725336+ferhacker@users.noreply.github.com>
2024-12-04 23:34:11 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
+const cors = require('cors');
 
 app.get("/", (req, res) => res.type('html').send(html));
 
