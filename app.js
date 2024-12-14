0ba42057cfd6f9c2fc9a064e18a5927fa146d41d
Fernando <111725336+ferhacker@users.noreply.github.com>
2024-12-14 11:35:17 -0300

    novomysql2.js

--- app.js
+++ app.js
@@ -1,55 +1,31 @@
-const express = require("express");
+const express = require('express');
+const mysql = require('mysql2');
+
 const app = express();
-const port = process.env.PORT || 3000;
-const cors = require('cors');
-const mysql = require('mysql');
+const port = 3000;
 
-app.use(cors());
-app.use(express.json());
-
-//app.get("/", (req, res) => res.type('html').send(html));
-
-const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
-
-// Configuração da conexão com o banco de dados
-const connection = mysql.createConnection({
+// Configurações da conexão com o banco de dados
+const pool = mysql.createPool({
   host: 'db4free.net',
   user: 'usuariofhacker',
   password: 'Bombanco515',
   database: 'meubanco_mysql'
 });
-//INSERT INTO `usuarios`(`id`, `name`, `phone`, `email`) VALUES ('[value-1]','[value-2]','[value-3]','[value-4]')
 
-connection.connect((err) => {
+// Rota para buscar os dados
+app.get('/usuarios', (req, res) => {
+  pool.query('SELECT id, name, phone, email FROM usuarios', (err, results) => {
     if (err) {
-        throw err;
+      console.error('Erro ao buscar dados:', err);
+      res.status(500).send('Erro ao buscar dados');
     } else {
-        console.log("Conexão com o banco de dados estabelecida com sucesso!");
+      console.log('Dados recebidos:', results);
+      res.json(results);
     }
-});
-
-// Rota para inserir um novo usuário
-app.post('/dados_usuario', (req, res) => {
-  const { name} = req.body;
-
-  connection.query('INSERT INTO usuarios (name) VALUES (?)',
-    [name],
-    (error, results) => {
-      if (error) {
-        console.error(error);
-        
-        res.status(500).send('Erro ao inserir usuário');
-      } else {
-        res.send('Usuário inserido com sucesso');
-      }
-    });
-   console.log(`tentei enviar`);
+  });
 });
 
 app.listen(port, () => {
   console.log(`Servidor rodando na porta ${port}`);
 });
-
-server.keepAliveTimeout = 120 * 1000;
-server.headersTimeout = 120 * 1000;
 
